f27ab6d7728447351d99dd2d4130d04c833042bd
Deepu Yalamanchili <deepu_yalamanchili@thbs.india.com>
2025-03-27 17:46:03 +0530

    Updated Register screen

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const Register = () => {
   return (
     <View>
-      <Text>Register</Text>
+      <Text>Register/User</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
