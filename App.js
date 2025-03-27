82efb6753635a09faa80b2bed546c40dc995d5fc
shivani <shivanihgf7@gmail.com>
2025-03-27 14:26:34 +0530

    projectSetup

---
+++ App.js
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+      <Text>App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
